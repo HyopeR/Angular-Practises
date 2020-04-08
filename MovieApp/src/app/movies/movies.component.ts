b14891a05b2a2c644b17d0fd0593179c4ed6cf40
HyopeR <tol_celik@hotmail.com>
2020-04-08 15:36:27 +0300

    Create Movie Component

--- MovieApp/src/app/movies/movies.component.ts
+++ MovieApp/src/app/movies/movies.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
+  // .movies = <div class="movies"></div>
   // tslint:disable-next-line:component-selector
   selector: 'movies',
   templateUrl: 'movies.component.html'
