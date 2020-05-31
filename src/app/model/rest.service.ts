d18b7aa86f626d7263d91384eddbcbbdd3a34663
HyopeR <tol_celik@hotmail.com>
2020-05-31 13:25:13 +0300

    Product and Categories models and Rest Service

---
+++ src/app/model/rest.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Product} from './product.model';
+import {Category} from './category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+  /*
+    model.module.ts dosyası içerinde HttpClientModule oluşturuldu.
+    Buradaysa constructor içinde kullanım için http'e aktarılıyor.
+   */
+  baseUrl: 'http://localhost:3500/';
+  constructor(private http: HttpClient) { }
+
+  /*
+    getProducts bir asenkron fonksiyondur. İşlemleri ona göre yapılmalıdır.
+  */
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl + 'products');
+  }
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.baseUrl + 'categories');
+  }
+}
