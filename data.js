334f978c07042370d2fad1a49de1efb5022cdf6d
HyopeR <tol_celik@hotmail.com>
2020-05-31 13:08:05 +0300

    Json-server fake web api

---
+++ data.js
@@ -0,0 +1,21 @@
+module.exports = function () {
+  return {
+    products: [
+      {id: 1, name: 'Samsung S5', price: 1000, imageUrl: '1.png', description: 'Good product.'},
+      {id: 2, name: 'Samsung S6', price: 2000, imageUrl: '2.png', description: 'Not bad product.'},
+      {id: 3, name: 'Samsung S7', price: 3000, imageUrl: '3.png', description: 'Bad product.'},
+      {id: 4, name: 'Samsung S8', price: 4000, imageUrl: '4.png', description: 'Popular product.'},
+      {id: 5, name: 'Samsung S9', price: 5000, imageUrl: '5.png', description: 'Useful product.'},
+      {id: 6, name: 'Samsung S10', price: 6000, imageUrl: '6.png', description: 'Like product.'},
+      {id: 7, name: 'Samsung S11', price: 7000, imageUrl: '7.png', description: 'Rarely product.'}
+    ],
+    categories: [
+      {id: 1, name: 'Telefon' },
+      {id: 2, name: 'Bilgisayar' },
+      {id: 3, name: 'Elektronik'}
+    ],
+    orders: [
+
+    ]
+  }
+};
