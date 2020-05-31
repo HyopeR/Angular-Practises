5316b3e101cb59d0311c7016c9cff7d562dcca9b
HyopeR <tol_celik@hotmail.com>
2020-05-31 08:32:22 +0300

    Project Created

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'root',
+  template: ` <div class="bg-primary m-5 p-5">
+    Shop Application
+  </div>`
+})
+export class AppComponent {
+}
