3af2798ab64727d3be385d3ca7f37a002ae55ef4
HyopeR <tol_celik@hotmail.com>
2020-08-07 20:20:38 +0300

    Order Save and Order Forms

--- src/app/model/order.model.ts
+++ src/app/model/order.model.ts
@@ -15,7 +15,8 @@ export class Order {
   constructor(public cart: Cart) { }
 
   clearOrder() {
-    this.id = this.name = this.address = this.city = this.phone = this.email = null;
+    this.id = null;
+    this.name = this.address = this.city = this.phone = this.email = null;
     this.isSent = false;
     this.cart.clear();
   }
--- src/app/shop/checkout/checkout.component.ts
+++ src/app/shop/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Order} from '../../model/order.model';
+import {OrderRepository} from '../../model/order.repository';
+import {NgForm} from '@angular/forms';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -8,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  orderSent: boolean = false;
+  submitted: boolean = false;
 
-  ngOnInit() {
+  constructor(
+    public order: Order,
+    private orderRepository: OrderRepository
+  ) { }
+
+  ngOnInit() { }
+
+  submitOrder(form: NgForm) {
+    this.submitted = true;
+
+    if (form.valid) {
+      this.orderRepository.saveOrder(this.order)
+        .subscribe(queueOrder => {
+          this.order.clearOrder();
+          this.orderSent = true;
+          this.submitted = false;
+        });
+    }
   }
 
 }
