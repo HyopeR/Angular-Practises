7ed996af9c8777382aa318e042f41a254761fabc
HyopeR <tol_celik@hotmail.com>
2020-08-09 08:35:19 +0300

    Admin product list get

--- src/app/admin/products/product-list/product-list.component.ts
+++ src/app/admin/products/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductRepository} from '../../../model/product.repository';
+import {Product} from '../../../model/product.model';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productRepository: ProductRepository) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  getProducts(): Product[] {
+    return this.productRepository.getProducts();
   }
 
 }
